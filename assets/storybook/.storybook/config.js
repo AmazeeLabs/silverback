3ab2d768964a293754ebf834d7048d62aa4cd197
Philipp Melab <philipp.melab@gmail.com>
2019-03-21 08:57:55 +0100

    Added twig-drupal-filters.

--- assets/storybook/.storybook/config.js
+++ assets/storybook/.storybook/config.js
@@ -2,6 +2,12 @@ import { addDecorator, configure } from '@storybook/html';
 import { withA11y } from '@storybook/addon-a11y';
 import '../scripts';
 
+import Twig from 'twig';
+import twigDrupal from 'twig-drupal-filters';
+
+// Add the filters to Drupal.
+twigDrupal(Twig);
+
 // Automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.(ts|js)?$/);
 function loadStories() {
@@ -9,6 +15,6 @@ function loadStories() {
 }
 
 // Helps make UI components more accessible.
-addDecorator(withA11y)
+addDecorator(withA11y);
 
 configure(loadStories, module);
--- assets/storybook/scripts.ts
+++ assets/storybook/scripts.ts
@@ -5,11 +5,3 @@ components.keys().forEach(filename => components(filename));
 
 const styles = require.context('./stories/twig', true, /\.css?$/);
 styles.keys().forEach(filename => styles(filename));
-
-import Twig from 'twig';
-
-// Fake translation filter, to mock Drupal's behavior.
-Twig.extendFilter("t", function(value) {
-    return value;
-});
-
