3067fbb6efb0aa10964b7f7b8d9d47672dcbd413
Philipp Melab <philipp.melab@gmail.com>
2019-03-21 09:30:53 +0100

    Removed examples from scaffolding.

--- assets/storybook/stories/twig/page/header/header.stories.ts
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from "@storybook/html";
-
-const Header = require('./header.html.twig');
-
-storiesOf('Page/Header', module)
-    .add('Simple', () => Header({
-        frontpage: '/',
-        sitename: 'AmazeeLabs'
-    }));
--- assets/storybook/stories/twig/page/page.stories.ts
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from "@storybook/html";
-
-const Page = require('./page.html.twig');
-
-
-storiesOf('Page', module)
-    .add('Simple', () => Page({content: "test"}));
