af30d83b96daa7ac5ed7a60b551294bc8ff93767
Philipp Melab <philipp.melab@gmail.com>
2019-07-03 16:37:53 +0200

    Added delivery test cases.

---
+++ cypress/32-workspaces-inheritance/index.js
@@ -0,0 +1,48 @@
+/* global Given, When, Then */
+
+import {pages, workspaces} from "../common/mappings";
+import {viewPage, createPage, editPage} from "../common/page";
+
+beforeEach(function () {
+  cy.prepareSnapshot('workspaces', function () {
+    cy.drush('scr cypress/integration/jira/SLB/common/00-workspaces.php');
+    cy.drush('cr');
+  });
+  cy.drush('cr');
+});
+
+
+Given(/^there is a page with title "([^"]*)" in the "([^"]*)" workspace$/, (title, workspace) => {
+  createPage(workspace, title);
+});
+
+When(/^an administrator accesses the "([^"]*)" page in the "([^"]*)" workspace$/, (title, workspace) => {
+  viewPage(workspace, title);
+});
+
+Then(/^the user sees a "([^"]*)" error$/, (error) => {
+  cy.contains(error);
+});
+
+
+When(/^an administrator accesses the "([^"]*)" page of the "([^"]*)" workspace$/, (page, workspace) => {
+  cy.drupalSession({user: "admin", workspace: workspaces[workspace]});
+  cy.visit(pages[page]);
+});
+
+Then(/^there should be no line for the page "([^"]*)"$/, (title) => {
+  cy.contains(title).should('not.exist');
+});
+
+Given(/^the title of "([^"]*)" has been changed to "([^"]*)" in the "([^"]*)" workspace$/, (oldTitle, newTitle, workspace) => {
+  editPage(workspace, oldTitle, newTitle)
+});
+
+When(/^the user clicks the title of the "([^"]*)" row$/, (title) => {
+  cy.contains(title).click();
+});
+
+Then(/^the displayed page title is "([^"]*)"$/, (title) => {
+  cy.get('h1').contains(title);
+});
+
